4f9088a38ea106ae2538ed4bd973f56697d3a3ac
Raphael Kletting <94311729+rqhi@users.noreply.github.com>
2024-03-31 23:42:13 +0200

    removed-link

--- frontend/src/UnapprovedEvents.js
+++ frontend/src/UnapprovedEvents.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import EventCard from './EventCard';
-import { Link } from 'react-router-dom';
 
 function UnapprovedEvents() {
   const [unapprovedEvents, setUnapprovedEvents] = useState([]);
